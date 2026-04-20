699d8fa5aec197acb1c9082492a2e7df306dc093
ranjinhai <ranjinhai@powerleader.com.cn>
2026-04-20 14:22:18 +0800

    fix: 增加fine tune上传

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 region=$(echo "$1"|cut -c1-3 )
-echo python main.py $region-s >"$region"s.log 
+
+echo python main.py $region-s 
 nohup python main.py -c $region-s >"$region"s.log &
 echo
 
